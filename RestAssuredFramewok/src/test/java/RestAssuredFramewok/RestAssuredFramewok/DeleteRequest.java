622fc26135ad437c200935c739b4dcbae13492b3
Nikhil boriwale <boriwale.n@gmail.com>
2019-02-23 23:40:55 +0530

    POST ,PUT ,DELETE Code Commit

---
+++ RestAssuredFramewok/src/test/java/RestAssuredFramewok/RestAssuredFramewok/DeleteRequest.java
@@ -0,0 +1,5 @@
+package RestAssuredFramewok.RestAssuredFramewok;
+
+public class DeleteRequest {
+
+}
---
+++ RestAssuredFramewok/src/test/java/RestAssuredFramewok/RestAssuredFramewok/PutRequest.java
@@ -0,0 +1,5 @@
+package RestAssuredFramewok.RestAssuredFramewok;
+
+public class PutRequest {
+
+}
